47fb9d745e8c4a333bb442601163184f8c400259
Anna Shishkina <shishki.ann@gmail.com>
2020-03-02 12:58:41 +0300

    localization

--- csp_data_localization_short.py
+++ csp_data_localization_short.py
@@ -31,7 +31,7 @@ import  matplotlib.pyplot as plt
 #            '0276', '0346', '0347', '0351', '0358', 
 #            '0380', '0381', '0382', '0383'] 
 #SUBJECTS = SUBJ_ASD + SUBJ_NT
-SUBJECTS = ['0106']
+SUBJECTS = ['0141']
 PATHfrom = '/net/server/data/Archive/aut_gamma/orekhova/KI/Scripts_bkp/Shishkina/KI/'
 subjects_dir = PATHfrom + 'freesurfersubjects'
 subjects_dir_case = PATHfrom + 'freesurfersubjects/Case'
@@ -64,71 +64,93 @@ for subject in SUBJECTS:
     original_data = mne.io.read_raw_fif(raw_fname, preload=False)
     original_info = original_data.info
     
-    ftname = savepath + subject + '/' + subject + '_fieldtrip_data_epochs.mat'
-    
+    ftname = savepath + subject + '/' + subject + '_preproc_alpha_2_40_epochs.mat'
+    fif_fname = '/net/server/data/Archive/aut_gamma/orekhova/KI/SUBJECTS/0141/ICA_nonotch_crop/epochs/0141-noerror-lagcorrected-epo.fif'
+    fast_epo_fif = mne.read_epochs(fif_fname, proj=False, verbose=None) 
+    epo_fif= mne.Epochs(fif_fname, tmin=-0.8, tmax=0)
+
     #load csp data for CSP1 fast
-    fast_epo = mne.read_epochs_fieldtrip(ftname, original_info, data_name='epochs_fast1', trialinfo_column=0)
-    fast_epo.save(subjpath + subject + 'epo_fast1.fif')
-    fast_fname = subjpath + subject + '_csp_epo_fast1.fif'
+    fast_epo = mne.read_epochs_fieldtrip(ftname, original_info, data_name='fast_alpha_bp', trialinfo_column=0)
+    fast_epo.save(PATHfrom + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/epochs/' + subject + '_epo_fast.fif')
+    fast_fname = PATHfrom + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/epochs/' + subject + '_epo_fast.fif'
     fast_epo = mne.read_epochs(fast_fname, proj=False, verbose=None) 
     
     #load csp data for CSP1 slow
-    slow_epo = mne.read_epochs_fieldtrip(ftname, original_info, data_name='epochs_slow1', trialinfo_column=0)
-    slow_epo.save(subjpath + subject + 'epo_slow1.fif')
-    slow_fname = subjpath + subject + 'epo_slow1.fif'
+    slow_epo = mne.read_epochs_fieldtrip(ftname, original_info, data_name='slow_alpha_bp', trialinfo_column=0)
+    slow_epo.save(PATHfrom + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/epochs/' + subject + '_epo_slow.fif')
+    slow_fname = PATHfrom + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/epochs/' + subject + '_epo_slow.fif'
     slow_epo = mne.read_epochs(slow_fname, proj=False, verbose=None) 
     
-    #load bandpassed for fast poststim
-    filename = subjpath + subject + '_preproc_alpha_bp_epochs.mat'
-    epo_fast = mne.read_epochs_fieldtrip(filename, original_info, data_name='fast_alpha_post', trialinfo_column=0)
-    epo_fast.save(subjpath + subject + '_epo_fast.fif')
-    epo_fast_fname = subjpath + subject + '_epo_fast.fif'
-    fast_epo_post = mne.read_epochs(epo_fast_fname, proj=False, verbose=None) 
+    raw_fname = '/net/server/data/Archive/aut_gamma/orekhova/KI/EmptyRoom/' + subject + '/er/' + subject + '_er1_sss.fif'
+    raw_noise = io.read_raw_fif(raw_fname, preload=True)
+    raw_noise.filter(2, 40, fir_design='firwin') 
+    noise_cov = mne.compute_raw_covariance(raw_noise, method='shrinkage')
     
-    #load bandpassed for fast poststim
-    filename = subjpath + subject + '_preproc_alpha_bp_epochs.mat'
-    epo_slow = mne.read_epochs_fieldtrip(filename, original_info, data_name='slow_alpha_post', trialinfo_column=0)
-    epo_slow.save(subjpath + subject + '_epo_slow.fif')
-    epo_slow_fname = subjpath + subject + '_epo_slow.fif'
-    slow_epo_post = mne.read_epochs(epo_slow_fname, proj=False, verbose=None)
-    
-    #compute noise cov
-    noise_cov_fast = mne.compute_covariance(fast_epo_post, method='shrinkage', rank=None)
-    noise_cov_slow = mne.compute_covariance(slow_epo_post, method='shrinkage', rank=None)
-    data_cov_fast = mne.compute_covariance(fast_epo, method='shrinkage', rank=None)
-    data_cov_slow = mne.compute_covariance(slow_epo, method='shrinkage', rank=None)
-    
-    #fig_cov, fig_spectra = mne.viz.plot_cov(noise_cov_fast, raw.info)
-    #fig_cov, fig_spectra = mne.viz.plot_cov(data_cov_slow, raw.info)
+#    savename_inv_fast = savepath + subject + '/' + subject + '_inv_fast'
+#    savename_inv_slow = savepath + subject + '/' + subject + '_inv_slow'
     
     info = fast_epo.info
-    inverse_operator_fast = make_inverse_operator(info, fwd, noise_cov_fast, loose=0.2, depth=0.8, verbose=True)
+    inverse_operator_fast = make_inverse_operator(info, fwd, noise_cov, loose=0.2, depth=0.8, verbose=True)
     
     info = slow_epo.info
-    inverse_operator_slow = make_inverse_operator(info, fwd, noise_cov_slow, loose=0.2, depth=0.8, verbose=True)
+    inverse_operator_slow = make_inverse_operator(info, fwd, noise_cov, loose=0.2, depth=0.8, verbose=True)
     
-    method = "MNE"
+    info = fast_epo_fif.info
+    inverse_operator_fif = make_inverse_operator(info, fwd, noise_cov, loose=0.2, depth=0.8, verbose=True)
+
+    method = "eLORETA"
     snr = 3.
     lambda2 = 1. / snr ** 2
     bandwidth =  'hann'
-    n_epochs_use = 57
-    stcs_fast = compute_source_psd_epochs(fast_epo[:n_epochs_use], inverse_operator_fast,
+    n_epochs_use = 49
+    stcs_slow = compute_source_psd_epochs(slow_epo[:n_epochs_use], inverse_operator_slow,
                                  lambda2=lambda2,
-                                 method=method, fmin=10, fmax=17,
+                                 method=method, fmin=8, fmax=13,
                                  bandwidth=bandwidth,
                                  return_generator=True, verbose=True)
-    
-    # compute average PSD over the first 10 epochs
+
     psd_avg = 0.
-    for i, stc in enumerate(stcs_fast):
-        psd_avg += stc.data
+    for i, stc_slow in enumerate(stcs_slow):
+        psd_avg += stc_slow.data
     psd_avg /= n_epochs_use
-    freqs = stc.times  # the frequencies are stored here
-    stc.data = psd_avg  
+    freqs = stc_slow.times  # the frequencies are stored here
+    stc_slow.data = psd_avg  
     
-    medium = (np.max(stc.data[:,1]))/2
-    maxim = np.max(stc.data[:,1])
-    brain = stc.plot(subject='Case'+subject, initial_time=12.4688, hemi='both', views='lat',  # 10 HZ
+    n_epochs_use = 38
+    stcs_fast = compute_source_psd_epochs(fast_epo[:n_epochs_use], inverse_operator_fast,
+                                 lambda2=lambda2,
+                                 method=method, fmin=2, fmax=40,
+                                 bandwidth=bandwidth,
+                                 return_generator=True, verbose=True)
+  
+    psd_avg = 0.
+    for i, stc_fast in enumerate(stcs_fast):
+        psd_avg += stc_fast.data
+    psd_avg /= n_epochs_use
+    freqs = stc_fast.times  # the frequencies are stored here
+    stc_fast.data = psd_avg  
+    
+    n_epochs_use = 184
+    stcs_fif = compute_source_psd_epochs(fast_epo_fif[:n_epochs_use], inverse_operator_fif,
+                                 lambda2=lambda2,
+                                 method=method, fmin=8, fmax=13,
+                                 bandwidth=bandwidth,
+                                 return_generator=True, verbose=True)
+
+    psd_avg = 0.
+    for i, stc_fif in enumerate(stcs_fif):
+        psd_avg += stc_fif.data
+    psd_avg /= n_epochs_use
+    freqs = stc_fif.times  # the frequencies are stored here
+    stc_fif.data = psd_avg  
+
+
+    stc = stc_fast
+    stc.data = stc_slow.data - stc_fast.data
+    
+    medium = (np.max(stc_fif.data[:,2]))/2
+    maxim = np.max(stc_fif.data[:,2])
+    brain = stc_fif.plot(subject='Case'+subject, initial_time=11.2219, hemi='both', views='lat',  # 10 HZ
                  clim=dict(kind='value', lims=(0, medium, maxim)), subjects_dir=subjects_dir, time_viewer = True)
     
     brain.save_image(savepath + subject + '/' + subject + '_fast.png')
