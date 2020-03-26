6b2c2e9be8d008cb954385952d54facdbce3cb30
Anna Shishkina <shishki.ann@gmail.com>
2020-03-26 20:27:00 +0300

    sloreta

--- csp_data_localization_short.py
+++ csp_data_localization_short.py
@@ -21,26 +21,18 @@ from mne.minimum_norm import make_inverse_operator, apply_inverse_epochs, source
 import  matplotlib.pyplot as plt
 
 #load subj info
-#SUBJ_NT = ['0101', '0102', '0103', '0104', '0105', '0136', '0137', '0138',
-#           '0140', '0158', '0162', '0163', '0178', '0179', '0255', '0257', '0348', 
-#           '0378', '0379', '0384']
-#                       
-#SUBJ_ASD = ['0106', '0107', '0139', '0141', '0159', '0160', '0161',  
-#            '0164', '0253', '0254', '0256', '0273', '0274', '0275',
-#            '0276', '0346', '0347', '0351', '0358', 
-#            '0380', '0381', '0382', '0383'] 
+SUBJ_NT = ['0101', '0102', '0103', '0104', '0105', '0136', '0137', '0138',
+           '0140', '0158', '0162', '0163', '0178', '0179', '0255', '0257', '0348', 
+           '0378', '0379', '0384']
+                       
+SUBJ_ASD = ['0106', '0107', '0139', '0141', '0159', '0160', '0161',  
+            '0164', '0253', '0254', '0256', '0273', '0274', '0275',
+            '0276', '0346', '0347', '0351', '0358', 
+            '0380', '0381', '0382', '0383'] 
 
 #File "/net/server/data/Archive/aut_gamma/orekhova/KI/freesurfersubjects/Case0107/bem/inner_skull.surf" does not exist
-#'0107', '0164', '0253', '0254', '0256', '0101', '0136', '0162', '0178', '0179', '0255',
-#'0378', '0379',
 
-#trans file "/net/server/data/Archive/aut_gamma/orekhova/KI/SUBJECTS/0273/ICA_nonotch_crop/0273_rings_ICA_raw-trans.fif" not found
-#'0107', '0164', '0253', '0254', '0256', '0101', '0136', '0162', '0178', '0179', '0255',
-#'0378', '0379', '0273', '0274', '0275', '0276', '0346', '0347', '0351', '0358', '0380', 
-#'0381', '0382', '0383', '0105', '0158', '0163', '0257', '0348','0384'
-
-#SUBJECTS = SUBJ_ASD + SUBJ_NT
-SUBJECTS = ['0104']
+SUBJECTS = SUBJ_ASD + SUBJ_NT
 PATHfrom = '/net/server/data/Archive/aut_gamma/orekhova/KI/'
 myPATH = '/net/server/data/Archive/aut_gamma/orekhova/KI/Scripts_bkp/Shishkina/KI/'
 subjects_dir = PATHfrom + 'freesurfersubjects'
@@ -50,7 +42,7 @@ for subject in SUBJECTS:
     subjpath = PATHfrom  + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/epochs/'
     savepath = myPATH + 'Results_Alpha_and_Gamma/'
     
-    trans = PATHfrom + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/' + subject + '_rings_ICA_raw-trans.fif'
+    trans = PATHfrom + 'TRANS/' + subject + '_rings_ICA_raw-trans.fif'
     
     #create bem model and make its solution
     conductivity = (0.3,)  # for single layer
@@ -81,108 +73,26 @@ for subject in SUBJECTS:
     epo_post_slow = epo_post["V1"]
     epo_post_fast = epo_post["V3"]
     
-    #load csp data for CSP1 fast from fieldtrip
-    #ftname = savepath + subject + '/' + subject + '_fieldtrip_data_epochs.mat'
-    #fast_epo = mne.read_epochs_fieldtrip(ftname, original_info, data_name='fast_alpha_bp', trialinfo_column=0)
-    #fast_epo.save(PATHfrom + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/epochs/' + subject + '_epo_fast.fif')
-    #fast_fname = PATHfrom + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/epochs/' + subject + '_epo_fast.fif'
-    #fast_epo = mne.read_epochs(fast_fname, proj=False, verbose=None) 
-    
-    #load csp data for CSP1 slow from fieldtrip
-    #slow_epo = mne.read_epochs_fieldtrip(ftname, original_info, data_name='slow_alpha_bp', trialinfo_column=0)
-    #slow_epo.save(PATHfrom + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/epochs/' + subject + '_epo_slow.fif')
-    #slow_fname = PATHfrom + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/epochs/' + subject + '_epo_slow.fif'
-    #slow_epo = mne.read_epochs(slow_fname, proj=False, verbose=None) 
-    
+
     #calculate noise covariance matrix from empty room data
     raw_fname = '/net/server/data/Archive/aut_gamma/orekhova/KI/EmptyRoom/' + subject + '/er/' + subject + '_er1_sss.fif'
     raw_noise = io.read_raw_fif(raw_fname, preload=True)
     raw_noise.filter(2, 40, fir_design='firwin') 
     noise_cov = mne.compute_raw_covariance(raw_noise, method='shrinkage')
     
-    #calculare inverse operator
-    #inverse_operator_fast = make_inverse_operator(fast_epo.info, fwd, noise_cov, loose=0.2, depth=None, verbose=True)
-    #inverse_operator_slow = make_inverse_operator(slow_epo.info, fwd, noise_cov, loose=0.2, depth=None, verbose=True)
-    
-#    #all
-#    inverse_operator_isi = make_inverse_operator(epo_isi.info, fwd, noise_cov, loose=0.2, depth=0.8, verbose=True)
-#    inverse_operator_post = make_inverse_operator(epo_post.info, fwd, noise_cov, loose=0.2, depth=0.8, verbose=True)
-##    
-    
     #slow
     inverse_operator_isi_slow = make_inverse_operator(epo_isi_slow.info, fwd, noise_cov, loose=0.2, depth=0.8, verbose=True)
-#    inverse_operator_post_slow = make_inverse_operator(epo_post_slow.info, fwd, noise_cov, loose=0.2, depth=0.8, verbose=True)
-##    
+    inverse_operator_post_slow = make_inverse_operator(epo_post_slow.info, fwd, noise_cov, loose=0.2, depth=0.8, verbose=True)
+   
     #fast
     inverse_operator_isi_fast = make_inverse_operator(epo_isi_fast.info, fwd, noise_cov, loose=0.2, depth=0.8, verbose=True)
-#    inverse_operator_post_fast = make_inverse_operator(epo_post_fast.info, fwd, noise_cov, loose=0.2, depth=0.8, verbose=True)
-##    
-    
+    inverse_operator_post_fast = make_inverse_operator(epo_post_fast.info, fwd, noise_cov, loose=0.2, depth=0.8, verbose=True)
+      
     method = "sLORETA"
     snr = 3.
     #lambda2 = 0.05
     lambda2 = 1. / snr ** 2
     bandwidth =  4.0
-    
-    #for slow epochs
-#    n_epochs_use = 49
-#    stcs_slow = compute_source_psd_epochs(slow_epo[:n_epochs_use], inverse_operator_slow,
-#                                 lambda2=lambda2,
-#                                 method=method, fmin=10, fmax=17,
-#                                 bandwidth=bandwidth,
-#                                 return_generator=True, verbose=True)
-#
-#    psd_avg = 0.
-#    for i, stc_slow in enumerate(stcs_slow):
-#        psd_avg += stc_slow.data
-#    psd_avg /= n_epochs_use
-#    freqs = stc_slow.times  # the frequencies are stored here
-#    stc_slow.data = psd_avg  
-    
-    #for fast epochs
-#    n_epochs_use = 38
-#    stcs_fast = compute_source_psd_epochs(fast_epo[:n_epochs_use], inverse_operator_fast,
-#                                 lambda2=lambda2,
-#                                 method=method, fmin=10, fmax=17,
-#                                 bandwidth=bandwidth,
-#                                 return_generator=True, verbose=True)
-#  
-#    psd_avg = 0.
-#    for i, stc_fast in enumerate(stcs_fast):
-#        psd_avg += stc_fast.data
-#    psd_avg /= n_epochs_use
-#    freqs = stc_fast.times  # the frequencies are stored here
-#    stc_fast.data = psd_avg  
-#    
-#    #for interstimulus epochs
-#    n_epochs_use = epo_isi.events.shape[0]
-#    stcs_isi = compute_source_psd_epochs(epo_isi[:n_epochs_use], inverse_operator_isi,
-#                                 lambda2=lambda2,
-#                                 method=method, fmin=10, fmax=17,
-#                                 bandwidth=bandwidth,
-#                                 return_generator=True, verbose=True)
-#    psd_avg = 0.
-#    for i, stc_isi in enumerate(stcs_isi):
-#        psd_avg += stc_isi.data
-#    psd_avg /= n_epochs_use
-#    freqs = stc_isi.times  # the frequencies are stored here
-#    stc_isi.data = psd_avg  
-#    
-#    
-#    #for stimulation period epochs
-#    n_epochs_use = epo_post.events.shape[0]
-#    stcs_post = compute_source_psd_epochs(epo_post[:n_epochs_use], inverse_operator_post,
-#                                 lambda2=lambda2,
-#                                 method=method, fmin=10, fmax=17,
-#                                 bandwidth=bandwidth,
-#                                 return_generator=True, verbose=True)
-#    psd_avg = 0.
-#    for i, stc_post in enumerate(stcs_post):
-#        psd_avg += stc_post.data
-#    psd_avg /= n_epochs_use
-#    freqs = stc_post.times  # the frequencies are stored here
-#    stc_post.data = psd_avg
-
 
     #for fast interstimulus epochs
     n_epochs_use = epo_isi_fast.events.shape[0]
@@ -198,19 +108,19 @@ for subject in SUBJECTS:
     freqs = stc_isi_fast.times  # the frequencies are stored here
     stc_isi_fast.data = psd_avg  
 
-#    #for fast stimulation period epochs
-#    n_epochs_use = epo_post_fast.events.shape[0]
-#    stcs_post_fast = compute_source_psd_epochs(epo_post_fast[:n_epochs_use], inverse_operator_post_fast,
-#                                 lambda2=lambda2,
-#                                 method=method, fmin=10, fmax=17,
-#                                 bandwidth=bandwidth,
-#                                 return_generator=True, verbose=True)
-#    psd_avg = 0.
-#    for i, stc_post_fast in enumerate(stcs_post_fast):
-#        psd_avg += stc_post_fast.data
-#    psd_avg /= n_epochs_use
-#    freqs = stc_post_fast.times  # the frequencies are stored here
-#    stc_post_fast.data = psd_avg
+    #for fast stimulation period epochs
+    n_epochs_use = epo_post_fast.events.shape[0]
+    stcs_post_fast = compute_source_psd_epochs(epo_post_fast[:n_epochs_use], inverse_operator_post_fast,
+                                 lambda2=lambda2,
+                                 method=method, fmin=10, fmax=17,
+                                 bandwidth=bandwidth,
+                                 return_generator=True, verbose=True)
+    psd_avg = 0.
+    for i, stc_post_fast in enumerate(stcs_post_fast):
+        psd_avg += stc_post_fast.data
+    psd_avg /= n_epochs_use
+    freqs = stc_post_fast.times  # the frequencies are stored here
+    stc_post_fast.data = psd_avg
 #    
     #for slow interstimulus epochs
     n_epochs_use = epo_isi_slow.events.shape[0]
@@ -226,30 +136,41 @@ for subject in SUBJECTS:
     freqs = stc_isi_slow.times  # the frequencies are stored here
     stc_isi_slow.data = psd_avg  
 
-#    #for slow stimulation period epochs
-#    n_epochs_use = epo_post_slow.events.shape[0]
-#    stcs_post_slow = compute_source_psd_epochs(epo_post_slow[:n_epochs_use], inverse_operator_post_slow,
-#                                 lambda2=lambda2,
-#                                 method=method, fmin=10, fmax=17,
-#                                 bandwidth=bandwidth,
-#                                 return_generator=True, verbose=True)
-#    psd_avg = 0.
-#    for i, stc_post_slow in enumerate(stcs_post_slow):
-#        psd_avg += stc_post_slow.data
-#    psd_avg /= n_epochs_use
-#    freqs = stc_post_slow.times  # the frequencies are stored here
-#    stc_post_slow.data = psd_avg
+    #for slow stimulation period epochs
+    n_epochs_use = epo_post_slow.events.shape[0]
+    stcs_post_slow = compute_source_psd_epochs(epo_post_slow[:n_epochs_use], inverse_operator_post_slow,
+                                 lambda2=lambda2,
+                                 method=method, fmin=10, fmax=17,
+                                 bandwidth=bandwidth,
+                                 return_generator=True, verbose=True)
+    psd_avg = 0.
+    for i, stc_post_slow in enumerate(stcs_post_slow):
+        psd_avg += stc_post_slow.data
+    psd_avg /= n_epochs_use
+    freqs = stc_post_slow.times  # the frequencies are stored here
+    stc_post_slow.data = psd_avg
 
     #subtract "baseline" (stimulation period) from interstimulus data 
-    stc = stc_isi_fast
-    stc.data = abs(stc_isi_fast.data - stc_isi_slow.data)/stc_isi_slow.data  
-    #stc_fast_VS_slow.data = stc_isi_fast.data - stc_isi_slow.data
+    stc_fast = stc_isi_fast
+    stc_fast.data = (stc_isi_fast.data - stc_post_fast.data)/stc_post_slow.data  
     
-    medium = (np.max(stc.data[:,3]))/2
-    maxim = np.max(stc.data[:,3])
-    brain = stc.plot(subject='Case'+subject, initial_time=14.9626, hemi='both', views='caud',  # 10 HZ
+    stc_slow = stc_isi_slow
+    stc_slow.data = (stc_isi_slow.data - stc_post_slow)/stc_post_fast.data
+    
+    #plot
+    medium = (np.max(stc_fast.data[:,3]))/2
+    maxim = np.max(stc_fast.data[:,3])
+    brain_fast = stc_fast.plot(subject='Case'+subject, initial_time=14.9626, hemi='both', views='caud',  # 10 HZ
                  clim=dict(kind='value', lims=(0, medium, maxim)), subjects_dir=subjects_dir)
+    brain_fast.save_image(savepath + subject + '/' + subject + 'isi_post_fast.png')
+    del brain_fast
+    
+    medium = (np.max(stc_slow.data[:,3]))/2
+    maxim = np.max(stc_slow.data[:,3])
+    brain_slow = stc_slow.plot(subject='Case'+subject, initial_time=14.9626, hemi='both', views='caud',  # 10 HZ
+                 clim=dict(kind='value', lims=(0, medium, maxim)), subjects_dir=subjects_dir)
+    
     #time_viewer = True
-    brain.save_image(savepath + subject + '/' + subject + 'isi_fastVSslow_15HZ_more_labmda.png')
-    del brain
+    brain_slow.save_image(savepath + subject + '/' + subject + 'isi_post_slow.png')
+    del brain_slow
     
