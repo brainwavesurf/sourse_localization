ac52130fdb847c15e7c4b54e0eab8b2955e986e6
Anna Shishkina <shishki.ann@gmail.com>
2020-04-13 10:46:48 +0300

    interstimulus power for 3 conditions filtered in 2-40HZ

--- absolute_power_of_meg_data.py
+++ absolute_power_of_meg_data.py
@@ -8,9 +8,6 @@ Created on Thu Apr  6 12:23:01 2020
 
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#import matplotlib 
-#get_ipython().run_line_magic('matplotlib', 'inline')
-#matplotlib .use('TKAgg') 
 #import packages
 import mne
 from mne import io
@@ -21,7 +18,7 @@ import numpy as np
 #load subj info
 SUBJ_NT = ['0101', '0102', '0103', '0104', '0105', '0136', '0137', '0138',
            '0140', '0158', '0162', '0163', '0178', '0179', '0255', '0257', '0348', 
-           '0378', '0379', '0384']
+           '0378', '0384']
                        
 SUBJ_ASD = ['0106', '0107', '0139', '0141', '0159', '0160', '0161',  
             '0164', '0253', '0254', '0256', '0273', '0274', '0275',
@@ -29,7 +26,7 @@ SUBJ_ASD = ['0106', '0107', '0139', '0141', '0159', '0160', '0161',
             '0380', '0381', '0382', '0383'] 
 
 SUBJECTS = SUBJ_ASD + SUBJ_NT
-#SUBJECTS = ['0102']
+
 PATHfrom = '/net/server/data/Archive/aut_gamma/orekhova/KI/'
 myPATH = '/net/server/data/Archive/aut_gamma/orekhova/KI/Scripts_bkp/Shishkina/KI/'
 subjects_dir = PATHfrom + 'freesurfersubjects'
@@ -38,19 +35,10 @@ for subject in SUBJECTS:
     
     subjpath = PATHfrom  + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/epochs/'
     savepath = myPATH + 'Results_Alpha_and_Gamma/'
-    trans = PATHfrom + 'TRANS/' + subject + '_rings_ICA_raw-trans.fif'
     
-    #create bem model and make its solution
-    conductivity = (0.3,)  # for single layer
-    model = mne.make_bem_model(subject='Case'+subject, ico=4, conductivity=conductivity, subjects_dir=subjects_dir)
-    bem = mne.make_bem_solution(model)
-    del model
     raw_fname = PATHfrom + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/' + subject + '_rings_ICA_raw.fif'
-    #set up a source space (inflated brain); if volume - use pos=5
-    src = mne.setup_source_space('Case' + subject, spacing='oct6', subjects_dir=subjects_dir, add_dist=False)
-    #make forward solution
-    fwd = mne.make_forward_solution(raw_fname, trans=trans, src=src, bem=bem, meg=True, eeg=False, mindist=5.0, n_jobs=2)
-    del bem, src
+    #load forward model
+    fwd = mne.read_forward_solution(savepath + subject + '/' + subject + '_fwd', verbose=None)
      
     #make epochs from raw    
     raw = io.Raw(raw_fname, preload=True)
@@ -79,13 +67,8 @@ for subject in SUBJECTS:
     medium_epo_isi = allepochs.__getitem__('V2')
     fast_epo_isi = allepochs.__getitem__('V3')
     
-    #calculate noise covariance matrix from empty room data
-    raw_fname = '/net/server/data/Archive/aut_gamma/orekhova/KI/EmptyRoom/' + subject + '/er/' + subject + '_er1_sss.fif'
-    raw_noise = io.read_raw_fif(raw_fname, preload=True)
-    raw_noise.filter(2, 40, fir_design='firwin') 
-    raw_noise.crop(0, 80)
-    methods = ['shrunk', 'empirical']
-    noise_cov = mne.compute_raw_covariance(raw_noise, method=methods, rank=dict(meg=69)) 
+    #load noise covariance matrix from empty room data
+    noise_cov = mne.read_cov(savepath + subject + '/' + subject + 'noise_cov_2_40Hz', verbose=None)
     
     #inverse operator
     inverse_operator = make_inverse_operator(allepochs.info, fwd, noise_cov, loose=0.2, depth=0.8, verbose=True)
