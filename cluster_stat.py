12c74fdc488994092c24f69324de5ff3937d56f3
Anna Shishkina <shishki.ann@gmail.com>
2020-04-14 14:24:35 +0300

    cluster analysis for the difference of two conditions between groups (NT and ASD)

--- cluster_stat.py
+++ cluster_stat.py
@@ -15,7 +15,6 @@ import numpy as np
 from scipy import stats as stats
 from mne.stats import summarize_clusters_stc, spatio_temporal_cluster_test
 
-from numpy.random import randn
 
 #load subj info
 SUBJ_NT = ['0101', '0102', '0103', '0104', '0105', '0136', '0137', '0138',
@@ -33,7 +32,7 @@ PATHfrom = '/net/server/data/Archive/aut_gamma/orekhova/KI/'
 myPATH = '/net/server/data/Archive/aut_gamma/orekhova/KI/Scripts_bkp/Shishkina/KI/'
 subjects_dir = PATHfrom + 'freesurfersubjects'
 
-for subject in SUBJECTS:
+for subject in SUBJ_ASD:
     
     subjpath = PATHfrom  + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/epochs/'
     savepath = myPATH + 'Results_Alpha_and_Gamma/'
@@ -48,31 +47,67 @@ for subject in SUBJECTS:
     #Apply morph to SourceEstimate
     stc_fsaverage_slow = morph.apply(stc_slow)
     stc_fsaverage_fast = morph.apply(stc_fast)
+    stc_fsaverage_diff = stc_fsaverage_slow
+    stc_fsaverage_diff.data = stc_fsaverage_fast.data - stc_fsaverage_slow.data
     
-    n_vertices_sample, n_times = stc_fsaverage_slow.data.shape
-    n_subjects = len(SUBJECTS)
+    n_vertices_fsave, n_times = stc_fsaverage_slow.data.shape
+    n_subjects = len(SUBJ_ASD)
+    #    Let's make sure our results replicate, so set the seed.
+    np.random.seed(0)
+    X1 = np.random.randn(n_vertices_fsave, n_times, n_subjects)
+    X1[:, :, :] += stc_fsaverage_diff.data[:, :, np.newaxis]
     
-    X = randn(n_vertices_sample, n_times, n_subjects, 2)
-    X[:, :, :, 0] += stc_fsaverage_slow.data[:, :, np.newaxis]
-    X[:, :, :, 1] += stc_fsaverage_fast.data[:, :, np.newaxis]
-
-X = np.abs(X)  # only magnitude
-X = X[:, :, :, 1] - X[:, :, :, 0]  # make paired contrast
-
-X = np.transpose(X, [2, 1, 0])
-
-connectivity = spatial_tris_connectivity(grade_to_tris(5)) 
-
-p_threshold = 0.001
-t_threshold = -stats.distributions.t.ppf(p_threshold / 2., n_subjects - 1)
-T_obs, clusters, cluster_p_values, H0 = clu = \
-    spatio_temporal_cluster_test(X, connectivity=connectivity, n_jobs=1, out_type='indices', buffer_size = None,
-                                       threshold=None, n_permutations=1000)
+for subject in SUBJ_NT:
     
+    subjpath = PATHfrom  + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/epochs/'
+    savepath = myPATH + 'Results_Alpha_and_Gamma/'
+    
+    #load stcs
+    stc_slow = mne.read_source_estimate(savepath + subject + '/' + subject + 'meg_slow_isi_2_40Hz')
+    stc_fast = mne.read_source_estimate(savepath + subject + '/' + subject + 'meg_fast_isi_2_40Hz')
+    
+    #Setting up SourceMorph for SourceEstimate
+    morph = mne.read_source_morph(savepath + subject + '/' + subject + 'morph_2_40-morph.h5')
+    
+    #Apply morph to SourceEstimate
+    stc_fsaverage_slow = morph.apply(stc_slow)
+    stc_fsaverage_fast = morph.apply(stc_fast)
+    stc_fsaverage_diff = stc_fsaverage_slow
+    stc_fsaverage_diff.data = stc_fsaverage_fast.data - stc_fsaverage_slow.data
+    
+    n_vertices_fsave, n_times = stc_fsaverage_slow.data.shape
+    n_subjects = len(SUBJ_NT)
+    #    Let's make sure our results replicate, so set the seed.
+    np.random.seed(0)
+    X2 = np.random.randn(n_vertices_fsave, n_times, n_subjects)
+    X2[:, :, :] += stc_fsaverage_diff.data[:, :, np.newaxis]
+    
+#    We want to compare the overall activity levels for each subject
+X1 = np.abs(X1)  # only magnitude
+X2 = np.abs(X2)  # only magnitude
+    
+X1 = np.transpose(X1, [2, 1, 0])
+X2 = np.transpose(X2, [2, 1, 0])
+
+X = [X1, X2]
+
+connectivity = spatial_tris_connectivity(grade_to_tris(5))
+
+#    Now let's actually do the clustering. This can take a long time...
+#    Here we set the threshold quite high to reduce computation.
+p_threshold = 0.0001
+f_threshold = stats.distributions.f.ppf(1. - p_threshold / 2.,
+                                        len(SUBJ_ASD) - 1, len(SUBJ_NT) - 1)
+
+T_obs, clusters, cluster_p_values, H0 = clu =\
+    spatio_temporal_cluster_test(X, connectivity=connectivity, n_jobs=1,
+                                 threshold=f_threshold)
+#    Now select the clusters that are sig. at p < 0.05 (note that this value
+#    is multiple-comparisons corrected).
 good_cluster_inds = np.where(cluster_p_values < 0.05)[0]
-tstep = stc_fsaverage_fast.tstep  
-fsave_vertices = [np.arange(10242), np.arange(10242)] 
 
+fsave_vertices = [np.arange(10242), np.arange(10242)]
+tstep = stc_fsaverage_slow.tstep
 stc_all_cluster_vis = summarize_clusters_stc(clu, tstep=tstep,
                                              vertices=fsave_vertices,
                                              subject='fsaverage5')
