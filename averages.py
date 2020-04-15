08c2b846789a4711a2b518e816ebaa9079fd293c
Anna Shishkina <shishki.ann@gmail.com>
2020-04-15 18:23:25 +0300

    fast-slow averages across subjects

--- averages.py
+++ averages.py
@@ -33,11 +33,11 @@ for subject in SUBJECTS:
     savepath = myPATH + 'Results_Alpha_and_Gamma/'
     
     #load stcs
-    stc_slow = mne.read_source_estimate(savepath + subject + '/' + subject + 'meg_slow_isi_10_17Hz')
-    stc_fast = mne.read_source_estimate(savepath + subject + '/' + subject + 'meg_fast_isi_10_17Hz')
+    stc_slow = mne.read_source_estimate(savepath + subject + '/' + subject + 'meg_slow_isi_2_40Hz')
+    stc_fast = mne.read_source_estimate(savepath + subject + '/' + subject + 'meg_fast_isi_2_40Hz')
 
     #Setting up SourceMorph for SourceEstimate
-    morph = mne.read_source_morph(savepath + subject + '/' + subject + 'morph_10_17-morph.h5')
+    morph = mne.read_source_morph(savepath + subject + '/' + subject + 'morph_2_40-morph.h5')
     
     #Apply morph to SourceEstimate
     stc_fsaverage_slow = morph.apply(stc_slow)
@@ -52,16 +52,15 @@ for subject in SUBJECTS:
     X[:, :, :, 1] += stc_fsaverage_slow.data[:, :, np.newaxis]
     
 X = np.abs(X)  # only magnitude
-X1 = (X[:, :, :, 0] - X[:, :, :, 1])/(X[:, :, :, 0] + X[:, :, :, 1])*100
-X2 = X[:, :, :, 0] - X[:, :, :, 1]
-X1_avg = np.average(X1,2)
-X2_avg = np.average(X2,2)
+X_avg = np.average(X,2)
+X1 = (X_avg[:, :, 0] - X_avg[:, :, 1])/(X_avg[:, :, 0] + X_avg[:, :, 1])*100
+X2 = X_avg[:, :, 0] - X_avg[:, :, 1]
 
 X_with = stc_fsaverage_slow
-X_with.data = X1_avg
+X_with.data = X1
 
 X_without = stc_fsaverage_fast
-X_without.data = X2_avg
+X_without.data = X2
 
-X_with.save(savepath + subject + '/'  + '10_17avg_with_norm')
-X_without.save(savepath + subject + '/' + '10_17avg_without_norm')
+X_with.save(savepath + subject + '/'  + '2_40avg_with_norm')
+X_without.save(savepath + subject + '/' + '2_40avg_without_norm')
