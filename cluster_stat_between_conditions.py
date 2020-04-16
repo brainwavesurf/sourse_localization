8de73c2283dd41f25a94984dbb8a6e149f56a170
Anna Shishkina <shishki.ann@gmail.com>
2020-04-16 12:17:22 +0300

    cluster analysis for all subjects between two conditions

--- cluster_stat_between_conditions.py
+++ cluster_stat_between_conditions.py
@@ -15,7 +15,6 @@ import numpy as np
 from scipy import stats as stats
 from mne.stats import summarize_clusters_stc, spatio_temporal_cluster_1samp_test
 
-
 #load subj info
 SUBJ_NT = ['0101', '0102', '0103', '0104', '0105', '0136', '0137', '0138',
            '0140', '0158', '0162', '0163', '0178', '0179', '0255', '0257', '0348', 
@@ -39,7 +38,7 @@ for subject in SUBJECTS:
     #load stcs
     stc_slow = mne.read_source_estimate(savepath + subject + '/' + subject + 'meg_slow_isi_2_40Hz')
     stc_fast = mne.read_source_estimate(savepath + subject + '/' + subject + 'meg_fast_isi_2_40Hz')
-    
+
     #Setting up SourceMorph for SourceEstimate
     morph = mne.read_source_morph(savepath + subject + '/' + subject + 'morph_2_40-morph.h5')
     
@@ -47,40 +46,37 @@ for subject in SUBJECTS:
     stc_fsaverage_slow = morph.apply(stc_slow)
     stc_fsaverage_fast = morph.apply(stc_fast)
     
+    #calculate dimensional parameters for X
     n_vertices_fsave, n_times = stc_fsaverage_slow.data.shape
     n_subjects = len(SUBJECTS)
-    #    Let's make sure our results replicate, so set the seed.
+    
+    #create a multi-dimensional array 
     np.random.seed(0)
     X = np.random.randn(n_vertices_fsave, n_times, n_subjects, 2) 
     X[:, :, :, 0] += stc_fsaverage_fast.data[:, :, np.newaxis]
     X[:, :, :, 1] += stc_fsaverage_slow.data[:, :, np.newaxis]
-    
+
+#calculate difference between fast(V3) and slow(V1) conditions   
 X = np.abs(X)  # only magnitude
-X = (X[:, :, :, 0] - X[:, :, :, 1])/(X[:, :, :, 0] + X[:, :, :, 1])*100
-    
+X = X[:, :, :, 0] - X[:, :, :, 1]
+
+#change to n_subjects, n_times, n_vertices
 X = np.transpose(X, [2, 1, 0])
 
+#computing connectivity
 connectivity = spatial_tris_connectivity(grade_to_tris(5))
 
-p_threshold = 0.001
-t_threshold = -stats.distributions.t.ppf(p_threshold / 2., n_subjects - 1)
+#set the threshold
+#p_threshold = 0.001
+#t_threshold = -stats.distributions.t.ppf(p_threshold / 2., n_subjects - 1)
 
+#do clustering
 T_obs, clusters, cluster_p_values, H0 = clu =\
-     spatio_temporal_cluster_1samp_test(X, connectivity=connectivity, n_jobs=1,
-                                 threshold=None)
-#    Now select the clusters that are sig. at p < 0.05 (note that this value
-#    is multiple-comparisons corrected).
+     spatio_temporal_cluster_1samp_test(X, connectivity=connectivity, n_jobs=1, threshold=None, buffer_size=None, verbose=True)
 
+#convenient representation of each cluster, where each cluster becomes a "time point" in the SourceEstimate
 fsave_vertices = [np.arange(10242), np.arange(10242)]
-tstep = stc_fsaverage_slow.tstep
-stc_all_cluster_vis = summarize_clusters_stc(clu, tstep=tstep,
-                                             vertices=fsave_vertices,
-                                             subject='fsaverage5')
-
+#tstep = stc_fsaverage_slow.tstep
+stc_all_cluster_vis = summarize_clusters_stc(clu, vertices=fsave_vertices, subject='fsaverage5')
+#save stc with clusters
 stc_all_cluster_vis.save(savepath + subject + '/' + subject + 'clusters_2_40_between_cond')
-
-good_cluster_inds = np.where(cluster_p_values < 0.05)[0]
-tt=[]
-for i in range(len(good_cluster_inds)):
-    tt += list(clusters[good_cluster_inds[i]][0])
-good_sources = np.asarray(tt) 
