e9555776d5a45a0a96fd018f2a7ca9d3ee2141b4
Anna Shishkina <shishki.ann@gmail.com>
2020-04-17 17:55:18 +0300

    fast-slow averages across subjects

--- averages.py
+++ averages.py
@@ -26,7 +26,8 @@ SUBJECTS = SUBJ_ASD + SUBJ_NT
 PATHfrom = '/net/server/data/Archive/aut_gamma/orekhova/KI/'
 myPATH = '/net/server/data/Archive/aut_gamma/orekhova/KI/Scripts_bkp/Shishkina/KI/'
 subjects_dir = PATHfrom + 'freesurfersubjects'
-
+X_with = []
+X_without = []
 for subject in SUBJECTS:
     
     subjpath = PATHfrom  + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/epochs/'
@@ -42,25 +43,24 @@ for subject in SUBJECTS:
     #Apply morph to SourceEstimate
     stc_fsaverage_slow = morph.apply(stc_slow)
     stc_fsaverage_fast = morph.apply(stc_fast)
-    
+    stc_str1 = morph.apply(stc_slow)
+    stc_str2 = morph.apply(stc_fast)
     n_vertices_fsave, n_times = stc_fsaverage_slow.data.shape
-    n_subjects = len(SUBJECTS)
-    #    Let's make sure our results replicate, so set the seed.
-    np.random.seed(0)
-    X = np.random.randn(n_vertices_fsave, n_times, n_subjects, 2) 
-    X[:, :, :, 0] += stc_fsaverage_fast.data[:, :, np.newaxis]
-    X[:, :, :, 1] += stc_fsaverage_slow.data[:, :, np.newaxis]
     
-X = np.abs(X)  # only magnitude
-X_avg = np.average(X,2)
-X1 = (X_avg[:, :, 0] - X_avg[:, :, 1])/(X_avg[:, :, 0] + X_avg[:, :, 1])*100
-X2 = X_avg[:, :, 0] - X_avg[:, :, 1]
+    stc_fsaverage_diff_with = ((stc_fsaverage_fast.data - stc_fsaverage_slow.data)/(stc_fsaverage_fast.data + stc_fsaverage_slow.data))*100
+    stc_fsaverage_diff_without = stc_fsaverage_fast.data - stc_fsaverage_slow.data
+    
+    X_with.append(stc_fsaverage_diff_with)
+    X_without.append(stc_fsaverage_diff_without)
+    
+X_with_avg = sum(X_with)/len(X_with)
+X_without_avg = sum(X_without)/len(X_without)
 
-X_with = stc_fsaverage_slow
-X_with.data = X1
+X_with = stc_str1
+X_with.data = X_with_avg
 
-X_without = stc_fsaverage_fast
-X_without.data = X2
+X_without = stc_str2
+X_without.data = X_without_avg
 
-X_with.save(savepath + subject + '/'  + '2_40avg_with_norm')
-X_without.save(savepath + subject + '/' + '2_40avg_without_norm')
+X_with.save(savepath + subject + '/'  + '2_40avg_with_norm_new')
+X_without.save(savepath + subject + '/' + '2_40avg_without_norm_new')
