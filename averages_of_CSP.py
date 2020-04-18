01d9cc12a9783b80fbd1b438e73a41d975dca6d3
Anna Shishkina <shishki.ann@gmail.com>
2020-04-18 15:07:24 +0300

    average_CSP_sum

--- averages_of_CSP.py
+++ averages_of_CSP.py
@@ -25,25 +25,32 @@ SUBJECTS = SUBJ_ASD + SUBJ_NT
 PATHfrom = '/net/server/data/Archive/aut_gamma/orekhova/KI/'
 myPATH = '/net/server/data/Archive/aut_gamma/orekhova/KI/Scripts_bkp/Shishkina/KI/'
 subjects_dir = PATHfrom + 'freesurfersubjects'
-X = []
+X_sum = []
+X_sum_norm = []
+X_avg = []
+X_avg_norm = []
 for subject in SUBJECTS:
     
     subjpath = PATHfrom  + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/epochs/'
     savepath = myPATH + 'Results_Alpha_and_Gamma/'
     
     #load stcs
-    sum_csp = mne.read_source_estimate(savepath + subject + '/' + subject + 'csp_sum_diff')
+    sum_csp = mne.read_source_estimate(savepath + '1_results/CSP_sum/' + subject + 'sum_CSP_V3-V1_10_17Hz')
 
     #Setting up SourceMorph for SourceEstimate
     morph = mne.read_source_morph(savepath + subject + '/' + subject + 'morph_CSP-morph.h5')
     
     #Apply morph to SourceEstimate
-    stc_fsaverage = morph.apply(sum_csp)
-    X.append(stc_fsaverage.data)
+    sum_csp_fsaverage = morph.apply(sum_csp)
+    #save
+    sum_csp_fsaverage.save(savepath + '1_results/CSP_sum_fsaverage5/' + subject + 'sum_CSP_V3-V1_10_17Hz')
     
-X_avg = sum(X)/len(X)
-#make stc format
-X_CSP = stc_fsaverage
-X_CSP.data = X_avg
-#save
-X_CSP.save(savepath + subject + '/'  + 'CSP_avg')
+    X_sum.append(sum_csp_fsaverage.data)
+    
+#average across subjects   
+X_avg_sum = sum(X_sum)/len(X_sum)
+
+#make stc format and save
+avg_CSP_sum = sum_csp_fsaverage
+avg_CSP_sum.data = X_avg_sum
+avg_CSP_sum.save(savepath + '1_results/average_CSP_sum/' + 'avg_sum_CSP')
