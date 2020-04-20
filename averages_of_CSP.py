a8aed9623527319e0514f5851b0fbcdfde72c806
Anna Shishkina <shishki.ann@gmail.com>
2020-04-20 11:01:40 +0300

    average_CSP_sum for two groups

--- averages_of_CSP.py
+++ averages_of_CSP.py
@@ -29,7 +29,7 @@ X_sum = []
 X_sum_norm = []
 X_avg = []
 X_avg_norm = []
-for subject in SUBJECTS:
+for subject in SUBJ_NT:
     
     subjpath = PATHfrom  + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/epochs/'
     savepath = myPATH + 'Results_Alpha_and_Gamma/'
@@ -53,4 +53,34 @@ X_avg_sum = sum(X_sum)/len(X_sum)
 #make stc format and save
 avg_CSP_sum = sum_csp_fsaverage
 avg_CSP_sum.data = X_avg_sum
-avg_CSP_sum.save(savepath + '1_results/average_CSP_sum/' + 'avg_sum_CSP')
+avg_CSP_sum.save(savepath + '1_results/average_CSP_sum/' + 'ASD_avg_sum_CSP')
+
+X_sum = []
+X_sum_norm = []
+X_avg = []
+X_avg_norm = []
+for subject in SUBJ_ASD:
+    
+    subjpath = PATHfrom  + 'SUBJECTS/' + subject + '/ICA_nonotch_crop/epochs/'
+    savepath = myPATH + 'Results_Alpha_and_Gamma/'
+    
+    #load stcs
+    sum_csp = mne.read_source_estimate(savepath + '1_results/CSP_sum/' + subject + 'sum_CSP_V3-V1_10_17Hz')
+
+    #Setting up SourceMorph for SourceEstimate
+    morph = mne.read_source_morph(savepath + subject + '/' + subject + 'morph_CSP-morph.h5')
+    
+    #Apply morph to SourceEstimate
+    sum_csp_fsaverage = morph.apply(sum_csp)
+    #save
+    sum_csp_fsaverage.save(savepath + '1_results/CSP_sum_fsaverage5/' + subject + 'sum_CSP_V3-V1_10_17Hz')
+    
+    X_sum.append(sum_csp_fsaverage.data)
+    
+#average across subjects   
+X_avg_sum = sum(X_sum)/len(X_sum)
+
+#make stc format and save
+avg_CSP_sum = sum_csp_fsaverage
+avg_CSP_sum.data = X_avg_sum
+avg_CSP_sum.save(savepath + '1_results/average_CSP_sum/' + 'NT_avg_sum_CSP')
